d18d4a874065ca401e5649fccd254ab63d17cd1c
guozhenyi <guozhenyi@yoyohr.com>
2020-06-19 14:07:52 +0800

    '注释掉百度统计'

--- usr/themes/default/awesome.js
+++ usr/themes/default/awesome.js
@@ -3,13 +3,13 @@
  * 载入百度统计
  * @date 2020-05-08
  */
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?77e2457bcca41617a8ce493efc17215a";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
+// var _hmt = _hmt || [];
+// (function() {
+//     var hm = document.createElement("script");
+//     hm.src = "https://hm.baidu.com/hm.js?77e2457bcca41617a8ce493efc17215a";
+//     var s = document.getElementsByTagName("script")[0];
+//     s.parentNode.insertBefore(hm, s);
+// })();
 
 
 /*
